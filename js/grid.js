b50aeb3ee95309adaca58abfae818cad4cddd4d8
JamesYech <jyech@jdtech.net>
2018-02-02 10:39:23 -0500

    Modified using jquery and minor syntax changes

--- js/grid.js
+++ js/grid.js
@@ -1,117 +1,97 @@
-
-var gridBackground;
+var gridColor;
 var tileColor;
-const gridLocation = document.getElementById('grid-cont');
 
 function setDefaults() {
+    //add listeners
+    $('#changeBtn').click(buildGrid);
+    $('#color-picker-tile').change(setNewTileColor);
+    $('#color-picker-grid').change(changeBackgroundColor);
+
     //Set color pickers -- has to be hex value
-    document.getElementById('background-color').value="#336b87";
-    document.getElementById('tile-color').value="#f9f9f9";
+    $('#color-picker-grid').val("#336b87");
+    $('#color-picker-tile').val("#f9f9f9");
 
     //set grid and tile colors to match pickers - as rgb
-    gridBackground=hexToRgb(document.getElementById('background-color').value);  //#d3d3d3
-    tileColor=hexToRgb(document.getElementById('tile-color').value);  //#f9f9f9
+    gridColor=hexToRgb($('#color-picker-grid').val());  //#d3d3d3
+    tileColor=hexToRgb($('#color-picker-tile').val());  //#f9f9f9
 
-    //create default grid based on values of input boxes
     buildGrid();
 }
 
+function buildGrid() {
+    //create default grid based on values of input boxes
+    let gridHeight=newHeight();
+    let gridWidth=newWidth();
+
+    //remove old grid-box - build new grid-box
+    $('#grid-box').remove();
+    $('#grid-cont').append('<div id="grid-box" class="row grid-box"></div>');
+    $('#grid-box').css({
+                        gridTemplateColumns : "repeat("+gridWidth+",1em",
+                        width : gridWidth+"em"
+                    });
+    $('#grid-box').on('click','div', toggleTileColor);
+
+    //add cells to grid-box
+    for (let i = 1; i <= (gridHeight * gridWidth); i++) {
+        $('#grid-box').append('<div></div>')
+    }
+
+    //set gridColor for all cells
+    $('#grid-box').children('div').each(function() {
+        $(this).css({backgroundColor: gridColor});
+    });
+}
+
+function changeBackgroundColor() {
+    let newBkg=hexToRgb($('#color-picker-grid').val());
+    let oldBkg;
+
+    //set oldBkg and verify in rgb format
+    (gridColor.length===7) ? oldBkg=hexToRgb(gridColor) : oldBkg=gridColor;
+
+    $('#grid-box').children('div').each(function() {
+        if($(this).css('backgroundColor')==="" || $(this).css('backgroundColor')===oldBkg) {
+           $(this).css({backgroundColor: newBkg});
+        }
+    });
+
+    gridColor=newBkg;
+}
+
+function toggleTileColor(tile) {
+    ($(this).css("background-color")===tileColor)
+        ? $(this).css({backgroundColor: gridColor})
+        : $(this).css({backgroundColor: tileColor})
+}
+
+function setNewTileColor() {
+    tileColor=hexToRgb($('#color-picker-tile').val());
+}
+
 function hexToRgb(color) {
-    if(color.length == 7) {
+    if(color.length === 7) {
         let r = parseInt(color.substr(1,2),16);
         let g = parseInt(color.substr(3,2),16);
         let b = parseInt(color.substr(5,2),16);
         return('rgb('+r+', '+g+', '+b+')');
-    } else console.log("bad hex color string");
-}
-
-function buildGrid() {
-    var newGrid = document.createElement('div');
-    var gridHeight=newHeight();
-    var gridWidth=newWidth();
-
-    if (gridHeight>25) {
-        //set gridheight and grid input box to 50
-        gridHeight=25;
-        document.getElementById("grht").value=25;
-    }
-
-    if (gridWidth>50) {
-        //set gridwidth and grid input box to 50
-                gridWidth=50;
-        document.getElementById("grwd").value=50;
-    }
-
-    document.getElementById("grid-box").remove();
-
-    for (var i = 1; i <= (gridHeight * gridWidth); i++) {
-        var newDiv = document.createElement('div');
-        newDiv.style.backgroundColor=gridBackground;
-        newGrid.appendChild(newDiv);
-    }
-
-    //var newGridWidth=gridWidth*1.067;
-    newGrid.id="grid-box";
-    newGrid.classList.add("row");
-    newGrid.classList.add("grid-box");
-    newGrid.style.gridTemplateColumns="repeat("+gridWidth+",1em)";
-    newGrid.style.width=gridWidth+"em";  //newGridWidth+"em";
-    newGrid.addEventListener('click', toggleTileColor);
-
-    gridLocation.appendChild(newGrid);
-
-    document.getElementById('grid-box')
-}
-
-
-function changeBackgroundColor() {
-
-    var newBkg=hexToRgb(document.getElementById('background-color').value);
-    var curBkg;
-    var grDivs=document.getElementById('grid-box').children;
-
-    if (gridBackground.length==7) {
-        var oldBkg=hexToRgb(gridBackground);
-    }   else { var oldBkg=gridBackground;
-        }
-
-    for (var i = grDivs.length - 1; i >= 0; i--){
-        curBkg=grDivs[i].style.backgroundColor;
-        if(curBkg=="" || curBkg==oldBkg) {
-            grDivs[i].style.backgroundColor=newBkg;
-        }
-    }
-
-    gridBackground=newBkg;
-}
-
-
-function toggleTileColor(tile) {
-
-    var curColor = tile.target.style.backgroundColor;
-
-    if(curColor==tileColor) {
-        tile.target.style.backgroundColor=gridBackground;
-    } else {
-        tile.target.style.backgroundColor=tileColor;
     }
 }
 
+function newHeight() {
+    if ($('#grht').val()>25) {
+        $('#grht').val(25);
+    }
+    return ($('#grht').val())
+}
 
-function setNewTileColor() {tileColor=hexToRgb(document.getElementById('tile-color').value);}
+function newWidth() {
+    if ($('#grwd').val()>50) {
+        $('#grwd').val(50);
+    }
+    return ($('#grwd').val())
+}
 
-function newHeight() {return(document.getElementById("grht").value);}
-
-function newWidth() {return(document.getElementById("grwd").value);}
-
-
-
-document.getElementById("grid-box").addEventListener('click', toggleTileColor);
-
-document.getElementById("myButton").addEventListener('click', buildGrid);
-
-document.getElementById("tile-color").addEventListener('change',setNewTileColor);
-
-document.getElementById("background-color").addEventListener('change',changeBackgroundColor);
-
+//this jquery is visually slower than the js line
+//$(document).ready(setDefaults);
 document.body.onload=setDefaults();
